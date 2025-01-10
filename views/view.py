568b96fbb581cd645e6b028c8c7e7b60960eafb0
Otávio Henrique <otaviohenriqdev@gmail.com>
2025-01-09 21:13:54 -0300

    Total gasto
    Erros de identação corrigidos, também foi adicionado uma função para o usuário conferir o total gasto com as assinaturas.

--- views/view.py
+++ views/view.py
@@ -1,6 +1,6 @@
 import __init__
 from models.database import engine
-from models.model import Subscription
+from models.model import Subscription, Payments
 from sqlmodel import Session, select
 from datetime import date
 
@@ -18,11 +18,47 @@ class SubscriptionSevice:
         with Session(self.engine) as session:
             statement = select(Subscription)
             results = session.exec(statement).all()
-        return results
+            return results
+
+    def delete(self, id):
+        
+
+    def _has_pay(self, results):
+        for result in results:
+            if result.date.month == date.today().month:
+                return True
+        return False
+
 
     def pay(self, subscription: Subscription):
-        ...
+        with Session(self.engine) as session:
+            statement = select(Payments).join(Subscription).where(Subscription.empresa==subscription.empresa)
+            results = session.exec(statement).all()
+            
+
+            if self._has_pay(results):
+                question = input('Essa conta já foi paga esse mês, você deseja pagar novamente?: Y / N ')
+
+                if not question.upper() == 'Y':
+                    return 
+                
+                
+
+            pay = Payments(subscription_id=subscription.id, date=date.today())
+            session.add(pay)
+            session.commit()
+
+    def total_value(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription)
+            results = session.exec(statement).all()
+
+        total = 0
+        for result in results:
+            total += result.valor
+
+        return float(total)
+            # print(f'{result.valor:.2f}')
 
 ss = SubscriptionSevice(engine)
-# subscription = Subscription(empresa='HBO Max', site='hbomax.com.br', data_assinatura=date.today(), valor=35)
-print(ss.list_all())
+print(ss.total_value())
